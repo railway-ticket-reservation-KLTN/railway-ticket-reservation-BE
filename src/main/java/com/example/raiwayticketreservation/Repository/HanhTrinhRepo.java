6a7fb75976d8957e2b07f99f346cb8a39552e370
hophuocduy2610 <hophuocduy261020@gmail.com>
2023-05-09 10:04:59 +0700

    Chỉnh sửa API Cập nhật hành trình

--- src/main/java/com/example/raiwayticketreservation/Repository/HanhTrinhRepo.java
+++ src/main/java/com/example/raiwayticketreservation/Repository/HanhTrinhRepo.java
@@ -1,13 +1,12 @@
 package com.example.raiwayticketreservation.Repository;
 
 import com.example.raiwayticketreservation.Entity.HanhTrinh;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-import java.sql.Time;
-import java.util.Date;
 import java.util.List;
-import java.util.Set;
 
 public interface HanhTrinhRepo extends JpaRepository<HanhTrinh, Long> {
     @Query(value = "SELECT * FROM railwayticketreservationdb.hanhtrinh " +
@@ -27,10 +26,12 @@ public interface HanhTrinhRepo extends JpaRepository<HanhTrinh, Long> {
             "AND ngay_den = ? AND gio_di = ? AND gio_den = ? AND ma_tau = ?", nativeQuery = true)
     public int kiemTraHanhTrinhTonTai(String gaDi, String gaDen, String ngayDi,
                                       String ngayDen, String gioDi, String gioDen, Long idTau);
-    @Query(value = "UPDATE railwayticketreservationdb.hanhtrinh" +
-            "SET ga_di = ?, ga_den = ?, gio_di = ?, gio_den = ?, " +
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE railwayticketreservationdb.hanhtrinh\n" +
+            "SET ga_di = ?, ga_den = ?, gio_di = ?, gio_den = ?,\n" +
             "ngay_di = ?, ngay_den = ?, gia_ve = ?, ma_tau = ?, trang_thai = 1\n" +
             "WHERE id = ?", nativeQuery = true)
-    public HanhTrinh capNhatHanhTrinh(String gaDi, String gaDen, String gioDi, String gioDen,
+    public void capNhatHanhTrinh(String gaDi, String gaDen, String gioDi, String gioDen,
                                       String ngayDi, String ngayDen, String giaVe, Long maTau, Long id);
 }
