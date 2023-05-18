46717ead28bcf3725bed92716ac002f215ed598a
hophuocduy2610 <hophuocduy261020@gmail.com>
2023-05-19 06:29:55 +0700

    Thêm API lấy danh sách tàu

--- src/main/java/com/example/raiwayticketreservation/Service/ServiceImpl/TauServiceImpl.java
+++ src/main/java/com/example/raiwayticketreservation/Service/ServiceImpl/TauServiceImpl.java
@@ -1,9 +1,12 @@
 package com.example.raiwayticketreservation.Service.ServiceImpl;
 
+import com.example.raiwayticketreservation.Entity.Tau;
 import com.example.raiwayticketreservation.Repository.TauRepo;
 import com.example.raiwayticketreservation.Service.TauService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TauServiceImpl implements TauService {
@@ -13,5 +16,10 @@ public class TauServiceImpl implements TauService {
     @Override
     public Long getIdTauTheoTenTau(String tenTau) {
         return tauRepo.getIdTauByTenTau(tenTau);
+    }
+
+    @Override
+    public List<Tau> getDanhSachTau() {
+        return tauRepo.findAll();
     }
 }
--- src/main/java/com/example/raiwayticketreservation/Service/TauService.java
+++ src/main/java/com/example/raiwayticketreservation/Service/TauService.java
@@ -1,5 +1,10 @@
 package com.example.raiwayticketreservation.Service;
 
+import com.example.raiwayticketreservation.Entity.Tau;
+
+import java.util.List;
+
 public interface TauService {
     public Long getIdTauTheoTenTau(String tenTau);
+    public List<Tau> getDanhSachTau();
 }
