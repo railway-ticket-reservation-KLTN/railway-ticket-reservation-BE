e63b64a66ebf0b32243e0836c2542d1bb5d52859
hophuocduy2610 <hophuocduy261020@gmail.com>
2023-06-02 10:47:31 +0700

    Fix MomoConfig

--- src/main/java/com/example/raiwayticketreservation/config/MomoConfig.java
+++ src/main/java/com/example/raiwayticketreservation/config/MomoConfig.java
@@ -15,5 +15,5 @@ public class MomoConfig {
 	public static String IPN_URL = "https://fcf6-123-24-233-164.ngrok.io";
 	public static String CREATE_ORDER_URL = "https://test-payment.momo.vn/v2/gateway/api/create";
 //	public static String REDIRECT_URL = "http://localhost:8080/v1/khachhang/thanhtoan/trangthai";
-	public static String REDIRECT_URL = "http://localhost:4200/momopayment";
+	public static String REDIRECT_URL = "https://sell-train-tickets.web.app/momopayment";
 }
